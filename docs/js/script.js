ee2b7351761a50f5721bac7f7f2ca81259fa6048
Maxwell Austensen <maxwell.austensen@nyu.edu>
2018-02-10 18:00:19 -0500

    add address -> violations and map

--- docs/js/script.js
+++ docs/js/script.js
@@ -7,62 +7,105 @@ L.tileLayer('https://b.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.p
 }).addTo(map);
 
 
+$('#addressForm').submit((event) => {
+
+  event.preventDefault();
+
+  // map.clearLayers();
+
+  const address = $('#address').val();
+
+  getBBL(address).then((property) => {
+
+    const bbl = splitBBL(property.bbl);
+
+    Promise.all([
+      getHpdJurisdiction(bbl),
+      getViolationInfo(bbl)
+    ])
+    .then((allData) => {
+        // All data available here in the order it was called.
+        const isReg = allData[0];
+        const violCounts = allData[1].violCounts;
+        outputViolInfo(isReg, violCounts);
+    });
+
+    map.flyTo(property.latLon, 18)
+
+    // circleOptions defined globally below
+    L.circleMarker(property.latLon, circleOptions).addTo(map)
+      .bindPopup('BBL: <b>' + property.bbl + '</b>');
+
+  });
+
+});
+
+
+
 $('#bblForm').submit((event) => {
 
   event.preventDefault();
 
-  $('#violationsMessage, #violationsCount').text('');
+  // map.clearLayers();
 
-  const boro = $('#boro').val();
-  const block = $('#block').val();
-  const lot = $('#lot').val();
+  const bbl = $('#bbl').val();
 
   Promise.all([
-      getHpdJurisdiction(boro, block, lot),
-      getViolationCount(boro, block, lot)
-    ])
-    .then((allData) => {
-      // All data available here in the order it was called.
+    getHpdJurisdiction(splitBBL(bbl)),
+    getViolationInfo(splitBBL(bbl))
+  ])
+  .then((allData) => {
+    // All data available here in the order it was called.
+    const isReg = allData[0];
+    const violCounts = allData[1].violCounts;
+    outputViolInfo(isReg, violCounts);
 
-      const isReg = allData[0];
-      const violCounts = allData[1];
+    const latLon = allData[1].latLon;
+    map.flyTo(latLon, 18)
+    // circleOptions defined globally below
+    L.circleMarker(latLon, circleOptions).addTo(map)
+      .bindPopup('BBL: <b>' + bbl + '</b>');
 
-      console.log(isReg);
-      console.log(violCounts);
-
-      if (violCounts['Total'] == 0 && !isReg) {
-
-        $('#violMessage').text('This property is not subject to HPD\'s jurisdiction');
-        $('.violClass, .violCount').text('');
-
-        return;
-      }
-
-      $('#violMessage').text('Number of Open Housing Code Violations:');
-
-      ['A', 'B', 'C'].forEach((i) => {
-        $('#violClass' + i).text('Class ' + i + ':');
-        $('#violCount' + i).text(violCounts[i]);
-        console.log(violCounts[i]);
-      });
-
-    });
+  });
 });
 
-const getViolationCount = (boro, block, lot) => {
 
+const outputViolInfo = (isReg, violCounts) => {
+
+  if (violCounts['Total'] == 0 && !isReg) {
+
+    $('#violMessage').text('This property is not subject to HPD\'s jurisdiction');
+    $('.violClass, .violCount').text('');
+
+    return;
+  }
+
+  $('#violMessage').text('Number of Open Housing Code Violations:');
+
+  ['A', 'B', 'C'].forEach((i) => {
+    $('#violClass' + i).text('Class ' + i + ':');
+    $('#violCount' + i).text(violCounts[i]);
+  });
+}
+
+
+const getViolationInfo = (bbl) => {
+
+  // https://data.cityofnewyork.us/Housing-Development/Housing-Maintenance-Code-Violations/wvxf-dwi5
   const url = 'https://data.cityofnewyork.us/resource/b2iz-pps8.json';
 
-  const query = `SELECT class` +
+  const query = `SELECT class, latitude AS lat, longitude AS lon` +
     ` WHERE violationstatus='Open'` +
-    `    AND boroid = ${boro} AND block = ${block} AND lot = ${lot}` +
-    ` |> SELECT class, COUNT(*) AS violCount` +
-    `    GROUP BY class`;
+    `    AND boroid = ${bbl.boro} AND block = ${bbl.block} AND lot = ${bbl.lot}` +
+    ` |> SELECT class, lat, lon, COUNT(*) AS violCount` +
+    `    GROUP BY class, lat, lon`;
 
   const processResp = (data) => {
 
+    const bblLatLon = [data[0].lat, data[0].lon];
+
     const violCounts = {'A': 0, 'B': 0, 'C': 0, 'Total': 0};
-    let count = 0;
+    let count = 0; //TODO: cut the 0 out
 
     data.forEach((row) => {
       count = parseInt(row.violCount);
@@ -70,19 +113,25 @@ const getViolationCount = (boro, block, lot) => {
       violCounts['Total'] += count;
     });
 
-    return violCounts;
+    const violInfo = {
+      'latLon': bblLatLon,
+      'violCounts': violCounts
+    }
+
+    return violInfo;
   }
 
   return getOpenData(url, query).then(processResp);
 }
 
 
-const getHpdJurisdiction = (boro, block, lot) => {
+const getHpdJurisdiction = (bbl) => {
 
+  // https://data.cityofnewyork.us/Housing-Development/Buildings-Subject-to-HPD-Jurisdiction/kj4p-ruqc
   const url = 'https://data.cityofnewyork.us/resource/sc6a-36xc.json';
 
   const query = `SELECT COUNT(*) AS regCount` +
-    ` WHERE boroid = ${boro} AND block = ${block} AND lot = ${lot}`;
+    ` WHERE boroid = ${bbl.boro} AND block = ${bbl.block} AND lot = ${bbl.lot}`;
 
   const processResp = (data) => {
     if (data[0].regCount == '0') {
@@ -102,14 +151,8 @@ const getOpenData = (url, query) => {
       type: 'GET',
       data: {'$query': query}
     })
-    .done((data) => {
-      console.log(data);
-      resolve(data);
-    })
-    .fail(() => {
-      console.log('request failed');
-      reject('request failed');
-    });
+    .done((data) => { resolve(data); })
+    .fail(() => { reject('request failed'); });
   });
   return promiseObj;
 }
@@ -122,7 +165,6 @@ const getBBL = (address) => {
       data: {'text': address}
     })
     .done((data) => {
-      // console.log(data);
       const lonLat = data.features[0].geometry.coordinates
 
       const property = {
@@ -132,33 +174,25 @@ const getBBL = (address) => {
 
       resolve(property);
     })
-    .fail(() => {
-      console.log('request failed');
-      reject('request failed');
-    });
+    .fail(() => { reject('request failed'); });
   });
   return promiseObj;
 }
 
 
-$('#addressForm').submit((event) => {
+const splitBBL = (bbl) => {
+  const bblObj = {
+    boro: bbl.substring(0, 1),
+    block: bbl.substring(1, 6),
+    lot: bbl.substring(6, 10)
+  };
+  return bblObj;
+}
 
-  event.preventDefault();
-
-  const address = $('#address').val();
-
-  getBBL(address).then((property) => {
-
-    const circleOptions = {
-      stroke: false,
-      radius: 7,
-      fillOpacity: 0.8,
-      fillColor: 'gold',
-      width: 0
-    }
-
-    L.circleMarker(property.latLon).addTo(map)
-        .bindPopup('<b>' + property.bbl + '</b>');
-  });
-
-});
+const circleOptions = {
+  stroke: false,
+  radius: 6,
+  fillOpacity: 0.9,
+  fillColor: 'gold',
+  width: 0
+};
